0cac8a18812934d0351bc24161928e83dc6468bc
August <augustaldred@bpp.com>
2019-11-19 09:17:00 +0000

    finished navbar an added heroes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+import Navbar from './components/Navbar'
+
 
 function App() {
   return (
-    <div className="App">
-      Hello from app
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+      </Fragment>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:type" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </Router>
   );
 }
 

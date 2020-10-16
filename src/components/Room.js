c8630b8ee43e19f551b4e51fb023ceb25d156032
August <hachigatsu14@outlook.com>
2020-10-16 15:10:25 +0100

    completed room data set up

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import defaultImg from '../images/details-1.jpeg';
 
 export default function Room({ room }) {
-    console.log(room)
-    const { name, slug, images, price } = room;
+
+    const { name, type, images, price } = room;
     return (
         <article className="room">
             <div className="img-container">
-                <img src={images[0]} alt="overall look of the single room" />
+                <img src={images[0] || defaultImg} alt="overall look of the single room" />
+                <div className="price-top">
+                    <p>${price}</p>
+                    <p>per night</p>
+                </div>
+                <Link to={`/rooms/${type}`} className="btn-primary room-link">Features</Link>
             </div>
+            <p className="room-info">{name}</p>
         </article>
     )
+}
+
+Room.propTypes = {
+    room: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        images: PropTypes.arrayOf(PropTypes.string).isRequired,
+        price: PropTypes.number.isRequired,
+    })
 }
